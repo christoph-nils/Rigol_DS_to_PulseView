4af87180676f6aad6c3f77429966cd5d14b48882
Christoph Nils <github@christoph-nils.de>
2018-01-25 02:16:45 +0100

    Add Project

---
+++ CSV_Converter/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CSV_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_load = new System.Windows.Forms.TextBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.txt_save = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_load
+            // 
+            this.txt_load.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_load.Location = new System.Drawing.Point(99, 14);
+            this.txt_load.Name = "txt_load";
+            this.txt_load.Size = new System.Drawing.Size(472, 20);
+            this.txt_load.TabIndex = 1;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(12, 41);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(81, 23);
+            this.btn_save.TabIndex = 2;
+            this.btn_save.Text = "SR speichern";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(12, 12);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(81, 23);
+            this.btn_load.TabIndex = 3;
+            this.btn_load.Text = "CSV laden";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_start.Location = new System.Drawing.Point(577, 41);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 4;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // txt_save
+            // 
+            this.txt_save.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_save.Location = new System.Drawing.Point(99, 43);
+            this.txt_save.Name = "txt_save";
+            this.txt_save.Size = new System.Drawing.Size(472, 20);
+            this.txt_save.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 81);
+            this.Controls.Add(this.txt_save);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.txt_load);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(8000, 120);
+            this.MinimumSize = new System.Drawing.Size(280, 120);
+            this.Name = "Form1";
+            this.Text = "Rigol CSV zu PulseView SR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_load;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.TextBox txt_save;
+    }
+}
+
---
+++ CSV_Converter/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+namespace CSV_Converter {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void btn_load_Click(object sender, EventArgs e) {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "CSV & Text|*.txt; *.csv|Alles|*";
+            if(fileDialog.ShowDialog() == DialogResult.OK) {
+                txt_load.Text = fileDialog.FileName;
+            }
+        }
+        private void btn_save_Click(object sender, EventArgs e) {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.Filter = "SR|*.sr|ZIP|*.zip";
+            if (fileDialog.ShowDialog() == DialogResult.OK) {
+                txt_save.Text = fileDialog.FileName;
+            }
+        }
+        private void btn_start_Click(object sender, EventArgs e) {
+            string filepath_input = txt_load.Text;
+            string filepath_output = txt_save.Text;
+            if (File.Exists(filepath_input)) {
+                if (File.Exists(filepath_output))
+                    File.Delete(filepath_output);
+                ZipArchive zipArchive = ZipFile.Open(filepath_output, ZipArchiveMode.Create);
+                int file_lenght = File.ReadAllLines(filepath_input).Length;
+                StreamReader reader = new StreamReader(filepath_input);
+                string[] parts = reader.ReadLine().Split(',');
+                int channels = parts.Length - 3;
+                string[] channel_names = new string[channels];
+                for(int i = 0; i < channels; i++)
+                    channel_names[i] = parts[i + 1];
+                parts = reader.ReadLine().Split(',');
+                double frequency;
+                frequency = 1/ Convert.ToDouble(parts[parts.Length - 1].Replace('.', ','));
+                string samplerate = Convert.ToInt32(frequency).ToString() + " Hz";
+                ZipArchiveEntry archiveEntry;
+                StreamWriter streamWriter; // generate some metadata
+                archiveEntry = zipArchive.CreateEntry("version");
+                streamWriter = new StreamWriter(archiveEntry.Open());
+                streamWriter.WriteLine("2");
+                streamWriter.Close();
+                streamWriter.Dispose();
+                archiveEntry = zipArchive.CreateEntry("metadata");
+                streamWriter = new StreamWriter(archiveEntry.Open());
+                streamWriter.WriteLine("[global]");
+                streamWriter.WriteLine("[device 1]");
+                streamWriter.WriteLine("samplerate = " + samplerate);
+                streamWriter.WriteLine("total analog = " + channels);
+                int channel_num = 1;
+                foreach(string channel_name in channel_names)
+                    streamWriter.WriteLine("analog" + channel_num++ + " = " + channel_name);
+                streamWriter.WriteLine("unitsize=1");
+                streamWriter.Close();
+                streamWriter.Dispose();
+                int byte_count = 4 * (file_lenght - 2); // generate buffer and other variables
+                byte[][] analog_output = new byte[channels][];
+                for (int i = 0; i < channels; i++)
+                    analog_output[i] = new byte[byte_count];                
+                int index = 0;
+                float value;
+                byte[] b;
+                while (!reader.EndOfStream) { // convert line by line to float and store values in byte arrays (easy to save)
+                    parts = reader.ReadLine().Split(',');
+                    for (int i = 0; i < channels; i++) {
+                        value = Convert.ToSingle(parts[i + 1].Replace('.', ','));
+                        b = BitConverter.GetBytes(value);
+                        analog_output[i][index +0] = b[0];
+                        analog_output[i][index +1] = b[1];
+                        analog_output[i][index +2] = b[2];
+                        analog_output[i][index +3] = b[3];
+                    }
+                    index += 4;
+                }
+                Stream stream_analog; // save compressed streams
+                for (int i = 0; i < channels; i++) {
+                    archiveEntry = zipArchive.CreateEntry("analog-1-" + (i + 1) + "-1");
+                    stream_analog = archiveEntry.Open();
+                    stream_analog.Write(analog_output[i], 0, byte_count);
+                    stream_analog.Close();
+                }
+                zipArchive.Dispose();
+            }
+        }
+    }
+}
+// in under 100 lines ;-)
